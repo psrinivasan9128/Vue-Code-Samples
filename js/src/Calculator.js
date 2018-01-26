fecbd0b143bf3bce3a237bae913254e02f59c46a
Prasanna Srinivasan <psrinivasan9128@gmail.com>
2018-01-26 16:56:22 -0500

    Add files via upload

---
+++ js/src/Calculator.js
@@ -0,0 +1,64 @@
+﻿var myCalculator = new Vue({
+    el: '#myVueCalculator',
+    data: {
+        inputData: '',
+        tempData : 0,
+        inputExpression: '',
+        actionMethod: ''
+    },
+    methods: {
+        Add: function (e) {
+            this.actionMethod = 'A';
+            this.tempData += parseInt( this.inputData);
+            this.inputExpression = this.inputExpression + ' ' + this.inputData + '+';
+            this.inputData = '';
+        },
+        Substract: function (e) {
+            this.actionMethod = 'S';
+            this.tempData -= parseInt(this.inputData);
+            this.inputExpression = this.inputExpression + ' ' + this.inputData + '-';
+            this.inputData = '';
+        },
+        Multiply: function (e) {
+            this.actionMethod = 'M';
+            if (this.tempData == 0) {
+                this.tempData = 1;
+            }
+            this.tempData *= parseInt(this.inputData);
+            this.inputExpression = this.inputExpression + ' ' + this.inputData + '*';
+            this.inputData = '';
+        },
+        Divide: function (e) {
+            this.actionMethod = 'D';
+            if (this.tempData == 0) {
+                this.tempData = parseInt(this.inputData);
+            }
+            else {
+                this.tempData = this.tempData / parseInt(this.inputData);
+            }
+            this.inputExpression = this.inputExpression + ' ' + this.inputData + '/';
+            this.inputData = '';
+        },
+        Clear: function (e) {
+            this.inputData = '';
+            this.tempData = 0;
+            this.inputExpression = '';
+        },
+        Total: function (e) {
+            switch (this.actionMethod) {
+                case "A":
+                    this.inputExpression = this.inputExpression + '   = ' + this.tempData;
+                    break;
+                case "S":
+                    this.inputExpression = this.inputExpression + '   = ' + this.tempData;
+                    break;
+                case "M":
+                    this.tempData = this.tempData * parseInt(this.inputData);
+                    this.inputExpression = this.inputExpression + '   = ' + this.tempData;
+                    break;
+            }
+
+            this.inputData = '';
+        }
+    }
+})
---
+++ js/src/app.js
@@ -0,0 +1,22 @@
+﻿//var growler = new Vue({
+//    el: '#growler',
+//    data: {
+//        beers: [
+//            { name: 'Ahool Ale', abv: 5.4 },
+//            { name: 'Sigbin Stout', abv: 8.1 }
+//        ]
+//    }
+//});
+
+var myFirstVue = new Vue({
+    el: '#myFirstView',
+    data: {
+        ViewName: 'hope this works',
+        query:''
+    },
+    methods: {
+        searchMe: function (t,e) {
+            alert(t);
+        }
+    }
+});
